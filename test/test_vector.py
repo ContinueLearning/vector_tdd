c0615365caf46a42c34e250358129d87eac74d69
Patrick Ayertey <patrick.ayertey@mckesson.com>
2018-07-13 09:23:21 -0500

    New changes

--- test/test_vector.py
+++ test/test_vector.py
@@ -1,4 +1,13 @@
 from src.vector import Vector
+
 def test_make_vector():
     v=Vector([1,2,3])
     assert len(v.nums) ==3
+
+def test_dimensions():
+    v = Vector([5,6,7,4])
+    assert v.dims == 4
+
+def test_norm():
+    v = Vector([3,4])
+    assert v.norm ==5
